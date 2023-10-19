332f5e06bfe9d0e7c7a12d1cfe42ecb937dca5fa
Veljko Andrijasevic <andrijasevicveljko1999@gmail.com>
2023-10-20 01:48:00 +0200

    Add screenshot method if test failed

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,13 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public abstract class BasicTest {
@@ -40,8 +46,16 @@ public abstract class BasicTest {
         driver.navigate().to(baseUrl);
     }
     @AfterMethod
-    public void afterMethod () {
+    public void afterMethod (ITestResult result) {
         driver.manage().deleteAllCookies();
+        if (ITestResult.FAILURE == result.getStatus()) {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(screenshot, new File("screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
     }
     @AfterClass
     public void afterClass(){
